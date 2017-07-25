cffd374c724b87aacfab0d97c9d5fedf206f6470
maronpinto <maronpinto@gmail.com>
2017-07-25 15:24:08 -0300

    Atualização do Chat

--- chat.py
+++ chat.py
@@ -18,13 +18,11 @@ def gravador(reg):
 def menu():
 	
 	os.system('clear')
-	print('                          CHAT                           ')
-	print('---------------------------------------------------------')
-	print(' 1-Chat  |  2-Listar  |  3-Nick   |  4-Status  |  0-Sair ')
-	print('---------------------------------------------------------\n')
+	print('                            CHAT                           ')
+	print('-----------------------------------------------------------')
+	print(' 1-Cadastro  |  2-Listar  |  3-Nick   |  4-Chat  |  0-Sair ')
+	print('-----------------------------------------------------------\n')
 
-	#opcao = input('Opcao => ')
-	#return(opcao)
 
 def servidor(port):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,44 +52,39 @@ def servidor(port):
 
 
 			if msg_lista[0] == '#REGISTRAR':
-
 				registro = dict(nome=msg_lista[1].strip(), IP=address[0], dia=dia_grava, hora=hora_grava, mensagem=msg.decode('utf-8'))
-
 				gravador(registro)
-
 				msg_ret = (msg_lista[1].strip())
-
 				data.send(msg_ret.encode('utf-8'))
 
 			if msg_lista[0] == '#LIST':
-
 				registro = dict(nome=msg_lista[1].strip(), IP=address[0], dia=dia_grava, hora=hora_grava, mensagem=msg.decode('utf-8'))
 				gravador(registro)
-
-
 				arq = open('chat.json', 'r')
 				arq_json = json.load(arq)
-
-				ret = ''
+				ret_lista = ''
 				for item in arq_json['chat']:
 					ret = ret + ',' + item['nome']
 
-				data.send(str(ret.lstrip(',')).encode('utf-8'))
+				data.send(str(ret).encode('utf-8'))
 
 			if msg_lista[0] == '#NICK':
-
 				registro = dict(nome=msg_lista[1].strip(), IP=address[0], dia=dia_grava, hora=hora_grava, mensagem=msg.decode('utf-8'))
 				gravador(registro)
-								
 				arq_nick = open('chat.json', 'r')
 				arq_dict = json.load(arq_nick)
-
 				for item in arq_dict['chat']:
 					if item['nome'] == msg_lista[1]:
 						ret = item['nome']
-
 				data.send(ret.encode('utf-8'))
-					
+
+
+			if msg_lista[0] == '#MENSAGEM':
+				registro = dict(nome=msg_lista[1].strip(), IP=address[0], dia=dia_grava, hora=hora_grava, mensagem=msg.decode('utf-8'))	
+				gravador(registro)				
+				ret = msg_lista[1]				
+				data.send(ret.encode('utf-8'))
+
 
 		print('Finalizando coneccao com o cliente {}'.format(address))
 		data.close()
@@ -123,16 +116,12 @@ def cliente(port):
 
 		elif opcao == '2':
 			menu()
-			print(' Listagem dos usarios')
-			
-			msg = '#LIST, ' + prompt
-			
+			print(' Listagem dos usarios')			
+			msg = '#LIST, ' + prompt			
 			sock.send(msg.encode('utf-8'))
 			retorno = sock.recv(1024)
-
 			ret_str = retorno.decode('utf-8')
 			ret_list = (ret_str.split(','))
-
 			for nomes in range(0, len(ret_list)):
 				print(str(nomes + 1) + ' - ' + ret_list[nomes])
 			
@@ -140,19 +129,33 @@ def cliente(port):
 			menu()
 			print(' Trocar nick')
 			nick = input(' Digite o novo nick=> ')
-			msg = '#NICK,' + nick
-			
+			msg = '#NICK,' + nick			
 			sock.send(msg.encode('utf-8'))
 			retorno = sock.recv(1024)
 			prompt = retorno.decode('utf-8')
+
+
+		elif opcao == '4':
+			menu()
+			print('Chat')
+			if prompt == '<>':
+				print('Voce tem que se registrar!')
+			else:
+				while msg != '0':
+					texto = input('<' + prompt +'> ')
+					msg = '#MENSAGEM, ' + texto
+					sock.send(msg.encode('utf-8'))
+					retorno = sock.recv(1024)
+					print('Servidor=> ' + retorno.decode('utf-8'))
+					print()
 
 		elif opcao == '0':
 			controle = '#QUIT'
 			break
 		else:
-			print('nao sei')
+			print('Procedimento invalido!')
 
-		opcao = input('<' + prompt + '> ')
+		opcao = input('<' + prompt + '>')
 		
 	sock.close()
 
